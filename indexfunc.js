b4b52f38e6cc424b6424bed58c1b24ac883dbbbe
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-04 10:24:44 +0200

    done

---
+++ indexfunc.js
@@ -0,0 +1,10 @@
+let Marbles = Number (prompt ('Enter the number of Marbles :'));
+let Children = Number (prompt ('Enter the number of Children :'));
+let op = '/';
+if (Marbles === Children){
+    alert('your answer is incorrect ');
+ }
+else {
+    alert(('your answer is '+ (Marbles / Children)));
+
+}
